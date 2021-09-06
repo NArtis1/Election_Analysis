7e3a12d68de321f6dfdfbd8dbd00a3c56c4fc5ec
NArtis1 <artis.natalie@gmail.com>
2021-09-05 22:45:09 -0400

    Adding the elction audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -12,7 +12,6 @@ total_votes = 0
 
 # 1. Declare a new list of candidates
 candidate_options = []
-
 # Declare empaty dictionary
 candidate_votes = {}
 
@@ -43,16 +42,38 @@ with open(file_to_load) as election_data:
             candidate_votes[candidate_name] = 0
         # Add a vote to that candidate's count
         candidate_votes[candidate_name] += 1
+
 # Print the candidate votes
     print(candidate_votes)
 
 # Determine the percentage of votes for each candidate by looping through the counts.
 # 1. Iterate through the candidate list.
-for candidate_name in candidate_votes:
+    for candidate_name in candidate_votes:
     # 2. Retrieve vote count of a candidate.
-    votes = candidate_votes[candidate_name]
-    # 3. Calculate the percentage of votes.
-    vote_percentage = float(votes) / float(total_votes) * 100
-    # 4. Print the candidate name and percentage of votes.
-    print(f"{candidate_name}: received {vote_percentage: .2f}% of the vote.")
+        votes = candidate_votes[candidate_name]
+    # 3. Calculate the percentage of votes
+        vote_percentage = float(votes) / float(total_votes) * 100
+    # Print each candidate, their voter count, and percentage to the terminal.
+        candidate_results = (f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        print(candidate_results)
 
+    # Determine winning vote count and candidate
+    # Determine if the votes are greater than the winning count.
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+     # 2. If true then set winning_count = votes and winning_percent =
+     # vote_percentage.
+            winning_count = votes
+            winning_percentage = vote_percentage
+     # 3. Set the winning_candidate equal to the candidate's name.
+            winning_candidate = candidate_name
+
+
+#  To do: print out the winning candidate, vote count and percentage to
+#   terminal.
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+print(winning_candidate_summary)
